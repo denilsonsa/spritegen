ab3e64a70d467608b2731ba60c9fda7f4ecfee60
Denilson Sá Maia <denilsonsa@gmail.com>
2016-08-13 18:17:16 -0300

    First working version.
    This is a complete conversion of the Java code of version 1.0 (2nd April
    2013) of Random Sprite Generator by Boris van Schooten;
    
    The code in this commit has essentially the same features as the Java
    code (except UI-related features, such as mass-generating several
    sprites or mutating a specific one; and no export features were
    implemented).

---
+++ README.md
@@ -0,0 +1,23 @@
+This is a conversion from Java to Javascript of [Random game sprite generator](http://tmtg.net/ludumdare/spritegen/) by [Boris van Schooten](http://tmtg.net/).
+
+http://denilsonsa.github.io/spritegen/spritegen.html
+
+Other links:
+
+* https://github.com/angarg12/RandomSpriteGenerator
+* http://img.uninhabitant.com/spritegen.html
+* http://eandegames.weebly.com/devlog/gamedev-resource-pick-random-sprite-generators
+* http://eandegames.com/gamedev-resource-pick-random-sprite-generators/
+
+TODO:
+
+* Create a better UI.
+* Add more sliders and toggles for fine-tuning the parameters.
+* Allow re-doing (and/or disabling) specific steps (the last function calls from `PixelArtGen.createSprite function`).
+    * This certainly requires clearing the `Sprite.pixels` to transparent.
+    * This might allow recoloring a sprite without changing the shape.
+    * Even better: visually display the intermediate steps.
+* Animate animated sprites.
+* Find out how to generate tile-based (background) graphics.
+    * Probably because the [jar file](http://tmtg.net/ludumdare/spritegen/SpriteGenerator.jar) is version 1.2, while the [source code](http://www.xs4all.nl/~borisvs/jgame-3.6.2.zip) is version 1.0.
+* Figure out the differences between [this](http://www.xs4all.nl/~borisvs/jgame-3.6.2.zip) and [that](https://github.com/angarg12/RandomSpriteGenerator).
---
+++ ui.js
@@ -0,0 +1,85 @@
+'use strict';
+
+////////////////////////////////////////////////////////////
+// Misc. useful functions.
+
+// Same behavior as Python's randrange().
+function randrange(begin, end) {
+	if (end === undefined) {
+		end = begin;
+		begin = 0;
+	}
+	return Math.floor(Math.random() * (end - begin) + begin);
+}
+
+////////////////////////////////////////////////////////////
+// UI interaction.
+
+function redraw_canvases() {
+	var w = g_sprite.width;
+	var h = g_sprite.height;
+
+	var rawcanvas = document.getElementById('raw');
+	var ctx = rawcanvas.getContext('2d');
+	rawcanvas.width = w;
+	rawcanvas.height = h;
+	ctx.imageSmoothingEnabled = false;
+	ctx.clearRect(0, 0, rawcanvas.width, rawcanvas.height);
+	g_imagedata = createImageDataFromSprite(g_sprite, ctx);
+	ctx.putImageData(g_imagedata, 0, 0);
+
+	var zoomedcanvas = document.getElementById('zoomed');
+	var zctx = zoomedcanvas.getContext('2d');
+	zoomedcanvas.width = w * 4;
+	zoomedcanvas.height = h * 4;
+	zctx.imageSmoothingEnabled = false;
+	zctx.clearRect(0, 0, zoomedcanvas.width, zoomedcanvas.height);
+	zctx.drawImage(rawcanvas,
+		0, 0,
+		w, h,
+		0, 0,
+		w * 4, h * 4);
+}
+
+function regenerate(ev) {
+	var shapeinput = document.getElementById('shape');
+	var colorinput = document.getElementById('color');
+
+	var shapenum = parseInt(shapeinput.value, 10);
+	var colornum = parseInt(colorinput.value, 10);
+
+	g_pixelartgen = shapes[shapenum];
+	g_colortable = coltables[colornum];
+	g_sprite = g_pixelartgen.createSprite(g_colortable);
+
+	redraw_canvases();
+}
+
+function init() {
+	var paramsform = document.getElementById('paramsform');
+	paramsform.addEventListener('input', regenerate);
+
+	var regenbutton = document.getElementById('regenbutton');
+	regenbutton.addEventListener('click', regenerate);
+
+	var shapeinput = document.getElementById('shape');
+	shapeinput.max = shapes.length - 1;
+	shapeinput.value = randrange(shapes.length);
+
+	var colorinput = document.getElementById('color');
+	colorinput.max = coltables.length - 1;
+	colorinput.value = randrange(coltables.length);
+
+	regenerate();
+}
+
+////////////////////////////////////////////////////////////
+// Global vars.
+
+var g_pixelartgen = null;
+var g_colortable = null;
+var g_sprite = null;
+var g_imagedata = null;
+
+// This script should be included with "defer" attribute.
+init();
